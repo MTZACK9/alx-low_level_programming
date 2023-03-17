102a8a76a4aabe271b919b1e6b4dda9e8e0caf0f
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-17 11:18:00 -0700

    All letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: all letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+			continue;
+		else
+			putchar(ch);
+	}
+	putchar('\n');
+}
