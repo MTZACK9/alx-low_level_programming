e5aee61c6ca07a32d2971288c86156b723951793
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-18 05:28:08 -0700

    combination of brains and materials.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: combination of brains and materials.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d1, d2;
+
+	for (d1 = 0; d1 < 9; d1++)
+	{
+		for (d2 = 1; d2 <= 9; d2++)
+		{
+			if (d1 < d2)
+			{
+				putchar(d1 + '0');
+				putachar(d2 + '0');
+
+				if (d1 != 8 || d2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
