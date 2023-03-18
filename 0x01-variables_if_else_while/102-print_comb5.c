6b1a4093c0fc9a4ffc5004902083109e056ddee7
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-18 14:11:27 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Software is eating the World.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+
+				putchar(' ');
+
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i != 99 || j != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
