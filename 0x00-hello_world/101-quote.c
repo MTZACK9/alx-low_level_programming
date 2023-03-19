69569d7b1e7f781f3b2ec9cac31c97c964fe9839
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-19 13:51:13 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: quote.
+ *
+ * Return: this time is 1
+ */
+
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, $q, 58);
+	return (1);
+}
