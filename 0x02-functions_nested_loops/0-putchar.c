b0044e905daa03ca544a0f2e36dca776eee766bb
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-21 04:22:23 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char putch[] = "_putchar";
+
+	for (int i = 0; i <= 7; i++)
+	{
+		_putchar(putch[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
