a80f61803df83bac22e35512d55f707dac832ebb
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-24 10:53:18 -0700

    Functions and nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ void print_square(int size)
 {
 	int row, column;
 
-	if(size > 0)
+	if (size > 0)
 	{
-		for (row = 1; row <= size; ++row)
+		for (row = 1; row <= size; row++)
 		{
 			for (column = 1; column <= size; ++column)
 				_putchar('#');
