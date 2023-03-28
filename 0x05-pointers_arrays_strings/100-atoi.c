b4190574735f53d4a8d778d7b7a715fc970ab26a
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-28 05:43:48 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi -  convert a string to an integer.
+ *
+ * @s: string input parameter
+ *
+ * Return: Integer
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
