4f191f3dfa9e642acfa18183ea4c7c668c7605a7
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-28 16:51:25 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+
+#define PASSWORD_LENGTH 8
+/**
+ * main - Entry point
+ *
+ * Description: program that generates random valid passwords.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+	const int charset_size = sizeof(charset) - 1;
+
+	srand(time(NULL));
+
+	int i;
+
+	for  (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % charset_size];
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+}
