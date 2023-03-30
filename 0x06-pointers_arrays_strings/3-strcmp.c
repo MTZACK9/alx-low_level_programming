0b460a9883567f5421abb722d895a52873e980a9
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-30 16:32:40 -0700

    Pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+/**
+ * _strcmp - function that compares two strings.
+ *
+ * @s1: First string input.
+ * @s2: Second string input.
+ *
+ * Return: The difference.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return *s1 - *s2;
+		}
+
+		s1++;
+		s2++;
+	}
+
+	return strlen(s1) - strlen(s2);
+}
