c5f294890a6f7880ecc78bc9b82ce239d5d0ff20
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-30 16:55:47 -0700

    Pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * reverse_array - function that reverses the content 
+ * 		of an array of integers.
+ *
+ * @a: pointer to array.
+ * @n: the number of elements of the array.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *start = a;
+	int *end = a + n - 1;
+
+	while (start < end)
+	{
+		int temp = *start;
+
+		*start = *end;
+		*end = temp;
+
+		start++;
+		end--;
+	}
+}
