f18bc35a2e529c8e079040399246c71d6406f5bf
Zakaria MAIT <ziko.mait14@gmail.com>
2023-03-30 17:43:54 -0700

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
 
 /**
  * cap_string - a function that capitalizes all words of a string
@@ -10,15 +8,31 @@
  * Return: Capitalized string.
 */
 
+
 char *cap_string(char *s)
 {
-	for (int i = 0; i < strlen(s); i++)
-	{
-		if (i == 0 || isspace(s[i - 1]) || strchr(",;.!?\"(){} ", s[i - 1]))
-		{
-			s[i] = toupper(s[i]);
-		}
-	}
+	int i = 0;
 
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			
+			if (i == 0)
+			{
+				s[i] -= 32;
+			}
+			
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
+			{
+				s[i] -= 32;
+			}
+		}
+		i++;
+	}
 	return (s);
 }
